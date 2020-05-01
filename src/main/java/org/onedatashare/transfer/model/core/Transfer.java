3a96e3227c00c8b60ed0fc6dabda0e52f2de576c
18vishal-singh <18vishal.singh@gmail.com>
2020-04-30 20:00:42 -0400

    Added Static Optimization

--- src/main/java/org/onedatashare/transfer/model/core/Transfer.java
+++ src/main/java/org/onedatashare/transfer/model/core/Transfer.java
@@ -3,8 +3,13 @@ package org.onedatashare.transfer.model.core;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import org.apache.commons.vfs2.FileContent;
+import org.apache.commons.vfs2.FileObject;
+import org.apache.commons.vfs2.FileSystemException;
+import org.apache.commons.vfs2.FileSystemManager;
 import org.onedatashare.transfer.model.TransferDetails;
 import org.onedatashare.transfer.model.drain.Drain;
+import org.onedatashare.transfer.model.error.transfer.NotAFileException;
 import org.onedatashare.transfer.model.request.TransferOptions;
 import org.onedatashare.transfer.model.tap.Tap;
 import org.onedatashare.transfer.model.util.Progress;
@@ -21,14 +26,19 @@ import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.nio.file.Files;
 
 
 @NoArgsConstructor
 @Service
 public class Transfer<S extends Resource, D extends Resource> {
+    protected FileSystemManager fileSystemManager;
+
     private String id;
     private S source;
     private D destination;
@@ -36,14 +46,16 @@ public class Transfer<S extends Resource, D extends Resource> {
     private TransferOptions options;
     private EntityInfo sourceInfo;
     private EntityInfo destinationInfo;
-    public static String fName ="";
+    public static String fName = "";
 
     private AtomicInteger concurrency = new AtomicInteger(5);
 
     private ArrayList<Disposable> disposableArrayList = new ArrayList<>();
     private static final Logger logger = LoggerFactory.getLogger(Transfer.class);
 
-    /** Periodically updated information about the ongoing transfer. */
+    /**
+     * Periodically updated information about the ongoing transfer.
+     */
     private final TransferInfo info = new TransferInfo();
 
     // Timer counts 0.0 for files with very small size
@@ -55,25 +67,27 @@ public class Transfer<S extends Resource, D extends Resource> {
     @Autowired
     TransferDetailsRepository transferDetailsRepository;
 
-    public Transfer(S source, D destination){
+    public Transfer(S source, D destination) {
         this.source = source;
         this.destination = destination;
     }
 
-  public Flux start(int sliceSize){
-        fName ="This is from transfer.java";
+    long fsize = 0l;
+
+    public Flux start(int sliceSize) {
+        fName = "This is from transfer.java";
 //      transferDetailsRepository.saveAll(Flux.just(new TransferDetails("abc",12l))).subscribe();
         logger.info("Within transfer start");
         return Flux.fromIterable(this.filesToTransfer)
                 .doOnSubscribe(s -> {
                     logger.info("Transfer started....");
-//                    print(fName);
                     this.startTime = Time.now();
                 })
-//                .parallel(2)
-//                .runOn(Schedulers.elastic())
+
+                .parallel(setParallelism())
+                .runOn(Schedulers.elastic())
                 .flatMap(file -> {
-                    logger.info("Transferring " + file.getPath());
+                    logger.info("Transferring----" + file.getPath() + file.getSize());
                     Tap tap;
                     try {
                         tap = this.source.getTap(this.sourceInfo, file);
@@ -93,7 +107,6 @@ public class Transfer<S extends Resource, D extends Resource> {
                     Drain finalDrain = drain;
                     return tap.openTap(sliceSize)
                             .doOnNext(slice -> {
-
                                 logger.info(file.getPath() + " slice received");
                                 try {
                                     finalDrain.drain(slice);
@@ -110,20 +123,39 @@ public class Transfer<S extends Resource, D extends Resource> {
                                 }
                             });
                 })
-//                .sequential()
+                .sequential()
                 .doOnComplete(() -> {
                     this.startTime = Time.now() - this.startTime;
-                    logger.info("Done transferring " + this.id + ". Took "+ startTime/1000 + " secs");
+                    logger.info("Done transferring " + this.id + ". Took " + startTime / 1000 + " secs");
 
                 });
     }
-//    public void print(String str){
-//        System.out.println(str);
-//
-//    }
-//    public void writeToDB(){
-//        transferDetailsRepository.saveAll(Flux.just(new TransferDetails(fName,12l))).subscribe();
-//    }
+
+    public int setParallelism() {
+        int parallelism = 2;
+        double totalSize = 0.0;
+
+        for (EntityInfo ei : filesToTransfer) {
+            System.out.println(ei.getPath()+"--"+ei.getSize());
+            totalSize += ei.getSize();
+        }
+        totalSize /= 1024;
+        totalSize /= 1024;
+
+        System.out.println("totalsize is -------------" + totalSize);
+        if (totalSize >= 0.25 && totalSize <= 25)
+            parallelism = 8;
+        else if (totalSize >= 26 && totalSize <= 100)
+            parallelism = 4;
+        else if (totalSize >= 101 && totalSize <= 600)
+            parallelism = 8;
+        else if (totalSize > 600)
+            parallelism = 5;
+        else
+            parallelism = 2;
+        System.out.println("Concurrency is : " + parallelism);
+        return parallelism;
+    }
 
 
     public TransferInfo addProgress(Slice slice) {
